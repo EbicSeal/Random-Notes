8f7c57db3c3525ab5be82d4bc032f585a0353b6a
Ruben <89786857+EbicSeal@users.noreply.github.com>
2023-11-19 08:40:42 +0100

    Update Better Forces Changes.lua

--- Better Forces Changes.lua
+++ Better Forces Changes.lua
@@ -1,1 +1,1 @@
-v0.02.1\nAdded sound replication\nAdded automatic aim repositioning\nAdded loadout saving\n
+v0.02.1 \nAdded sound replication \nAdded automatic aim repositioning \nAdded loadout saving \n
